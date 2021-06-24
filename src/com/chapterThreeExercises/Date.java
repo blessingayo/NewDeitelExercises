2c47d46722e5ced8b15878d4f630232ed726dc25
Blessing Asuelimen <asuelimenblessing630@gmail.com>
2021-06-23 19:05:12 -0700

    refactored

--- src/com/chapterThreeExercises/Date.java
+++ src/com/chapterThreeExercises/Date.java
@@ -1,2 +1,54 @@
-package com.chapterThreeExercises;public class Date {
+//3.14 (Date Class) Create a class called Date that includes three instance variables—a month (type
+//        int), a day (type int) and a year (type int). Provide a constructor that initializes the three instance
+//        variables and assumes that the values provided are correct. Provide a set and a get method for each
+//        instance variable. Provide a method displayDate that displays the month, day and year separated
+//        by forward slashes (/). Write a test app named DateTest that demonstrates class Date’s capabilities.
+
+
+package com.chapterThreeExercises;
+
+public class Date {
+    private int month;
+    private int day;
+    private int year;
+    private final int displayDate;
+
+    public Date(int month, int day, int year, int displayDate){
+        this.month = month;
+        this.day = day;
+        this.year = year;
+        this.displayDate = displayDate;
+    }
+
+
+    public void setMonth(int birthMonth) {
+        month = birthMonth;
+
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public void setDay(int birthDay) {
+        day = birthDay;
+    }
+
+    public int getDate() {
+        return day;
+    }
+
+    public void setYear(int birthYear) {
+        year = birthYear;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public int displayDate() {
+        System.out.println("display date " + month + "/" + day + "/" + year);
+        return displayDate;
+    }
 }
+
--- test/com/chapterThreeExercises/DateTest.java
+++ test/com/chapterThreeExercises/DateTest.java
@@ -1,4 +1,40 @@
+package com.chapterThreeExercises;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class DateTest {
-  
+    Date myDate;
+
+    @BeforeEach
+    void setUp() {
+        myDate = new Date(8, 30, 2000, 8 / 30 / 2000);
+    }
+    @Test
+    void testThatDateCanSetMonth(){
+        myDate.setMonth(8);
+        assertEquals(8, myDate.getMonth());
+    }
+    @Test
+    void testThatAppCanSetDay(){
+        myDate.setDay(30);
+        assertEquals(30, myDate.getDate());
+    }
+    @Test
+    void testThatAppCanSetYear(){
+        myDate.setYear(2000);
+        assertEquals(2000, myDate.getYear());
+    }
+    @Test
+    void testThatAppCanDisplayDate(){
+        myDate.setMonth(8);
+        myDate.setDay(30);
+        myDate.setYear(2000);
+        assertEquals(8 / 30 / 2000, myDate.displayDate());
+
+
+    }
+
 }
